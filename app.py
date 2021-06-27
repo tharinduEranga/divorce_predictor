ce2d3af144c2ec10f4c90da423e1441958647e7c
tharinduEranga <tharindueranga3@gmail.com>
2021-06-27 23:10:41 +0530

    fixed pca error

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask import request
 
 import pandas as pd
+import numpy as np
 from sklearn.decomposition import PCA # Import PCA for Feature Reduction
+import xgboost as xgb # Import XGboost Classifier
 
 import pickle
 
@@ -14,16 +16,16 @@ def hello_world():
 	content = request.json
 	print(content)
 
-	model = pickle.load(open('divorce_r_forest', 'rb'))
+	model = pickle.load(open('divorce_xgbc', 'rb'))
 	print("Model: {}", model)
-	ll = ['2', '4', '1', '2', '3', '2', '4', '5', '2', '3', '2', '4', '1', '2', '3', '2', '4', '5', '2', '3']
+	ll = content
 	ll3 = [ll]
 	print("ll3: {}", ll3)
 	scaler = pickle.load(open('divorce_scaler', 'rb'))
-	print("scaler: {}", scaler)
-	ll2 = scaler.transform(ll3)
+	#ll2 = scaler.transform(ll3)
+	ll2 = ll3
 	print("ll2: {}", ll2)
-	# ll2 = pd.DataFrame(ll2)
+	ll2 = pd.DataFrame(ll2)
 	ll2 = reduce_features(ll2)
 	prediction = model.predict(ll2)
 	print("prediction: {}", prediction)
@@ -37,8 +39,7 @@ def reduce_features(features):
 	n_components = 8
 	# n_components is no of dimensions to retain
 	print("features_count: {}", features.shape[1])
-	pca = PCA(n_components=n_components) # Principal Component Analysis technique
-	pca.fit(features)
+	pca = pickle.load(open('divorce_pca', 'rb')) # Principal Component Analysis technique
 	features_reduced = pd.DataFrame(pca.transform(features), index=features.index,
 									columns=["PC" + str(i) for i in range(1, n_components + 1)])
 
